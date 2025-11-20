25355c8d0599134f695e099ea51b732829e765cd
procendp <procendp@gmail.com>
2025-11-21 01:36:13 +0900

    Force load HTTP requests in WSGI before Django app loading

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+# CRITICAL: HTTP requests 라이브러리를 Django 로드 전에 먼저 import
+import sys
+import requests as _http_requests_lib
+sys.modules['http_lib'] = _http_requests_lib
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
